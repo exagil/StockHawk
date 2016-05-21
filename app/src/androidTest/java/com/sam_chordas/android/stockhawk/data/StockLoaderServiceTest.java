b07b016d936cb1c4cf22a989792d26a3a78259ee
Chirag <me@chi6rag.net>
2016-05-21 10:01:43 +0530

    Loading quote symbol when quote present locally

--- app/src/androidTest/java/com/sam_chordas/android/stockhawk/data/StockLoaderServiceTest.java
+++ app/src/androidTest/java/com/sam_chordas/android/stockhawk/data/StockLoaderServiceTest.java
@@ -1,5 +1,6 @@
 package com.sam_chordas.android.stockhawk.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.test.AndroidTestCase;
 
@@ -23,6 +24,22 @@ public class StockLoaderServiceTest extends AndroidTestCase {
         StockLoaderService stockLoaderService = new StockLoaderService(getContext());
         String actualSymbolName = stockLoaderService.loadQuoteSymbolForQuoteId(1);
         assertEquals(null, actualSymbolName);
+    }
+
+    @Test
+    public void shouldLoadQuoteSymbolWhenCorrespondingQuoteExistsLocally() {
+        StockLoaderService stockLoaderService = new StockLoaderService(getContext());
+        ContentValues quoteContentValues = new ContentValues();
+        quoteContentValues.put(QuoteColumns.SYMBOL, "YHOO");
+        quoteContentValues.put(QuoteColumns.PERCENT_CHANGE, "20");
+        quoteContentValues.put(QuoteColumns.CHANGE, "10");
+        quoteContentValues.put(QuoteColumns.BIDPRICE, "10");
+        quoteContentValues.put(QuoteColumns.CREATED, "20-10-2016");
+        quoteContentValues.put(QuoteColumns.ISUP, 1);
+        quoteContentValues.put(QuoteColumns.ISCURRENT, 1);
+        getContext().getContentResolver().insert(QuoteProvider.Quotes.CONTENT_URI, quoteContentValues);
+        String actualSymbolName = stockLoaderService.loadQuoteSymbolForQuoteId(1);
+        assertEquals("YHOO", actualSymbolName);
     }
 
     private void resetDatabase() {
--- app/src/main/java/com/sam_chordas/android/stockhawk/data/StockLoaderService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/data/StockLoaderService.java
@@ -2,6 +2,7 @@ package com.sam_chordas.android.stockhawk.data;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 
 public class StockLoaderService {
     private final ContentResolver contentResolver;
@@ -11,6 +12,14 @@ public class StockLoaderService {
     }
 
     public String loadQuoteSymbolForQuoteId(int quoteId) {
-        return null;
+        Cursor quoteSymbolQuery = contentResolver.query(QuoteProvider.Quotes.CONTENT_URI,
+                new String[]{QuoteColumns.SYMBOL},
+                "_id=?",
+                new String[]{String.valueOf(quoteId)},
+                null
+        );
+        if (!quoteSymbolQuery.moveToFirst())
+            return null;
+        return quoteSymbolQuery.getString(quoteSymbolQuery.getColumnIndex(QuoteColumns.SYMBOL));
     }
 }
