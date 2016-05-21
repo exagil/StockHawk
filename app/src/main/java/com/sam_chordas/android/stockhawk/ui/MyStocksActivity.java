4cb4893aa0d35676938f2a313f1e92eb8743a6fd
Chirag <me@chi6rag.net>
2016-05-21 07:53:32 +0530

    Launching detail activity with details of the particular stock

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -8,9 +8,9 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
@@ -22,18 +22,19 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.PeriodicTask;
+import com.google.android.gms.gcm.Task;
+import com.melnykov.fab.FloatingActionButton;
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteDatabase;
 import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 import com.sam_chordas.android.stockhawk.rest.QuoteCursorAdapter;
 import com.sam_chordas.android.stockhawk.rest.RecyclerViewItemClickListener;
 import com.sam_chordas.android.stockhawk.rest.Utils;
 import com.sam_chordas.android.stockhawk.service.StockIntentService;
 import com.sam_chordas.android.stockhawk.service.StockTaskService;
-import com.google.android.gms.gcm.GcmNetworkManager;
-import com.google.android.gms.gcm.PeriodicTask;
-import com.google.android.gms.gcm.Task;
-import com.melnykov.fab.FloatingActionButton;
 import com.sam_chordas.android.stockhawk.touch_helper.SimpleItemTouchHelperCallback;
 
 public class MyStocksActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -87,7 +88,9 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
                 new RecyclerViewItemClickListener.OnItemClickListener() {
                     @Override
                     public void onItemClick(View v, int position) {
-                        startActivity(new Intent(getApplicationContext(), StockDetailActivity.class));
+                        Intent stockDetailIntent = new Intent(getApplicationContext(), StockDetailActivity.class);
+                        stockDetailIntent.putExtra(QuoteColumns._ID, mCursorAdapter.getItemId(position));
+                        startActivity(stockDetailIntent);
                     }
                 }));
         recyclerView.setAdapter(mCursorAdapter);
--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivity.java
@@ -1,12 +1,27 @@
 package com.sam_chordas.android.stockhawk.ui;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 
 public class StockDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        long quoteId = getIntent().getLongExtra(QuoteColumns._ID, 0);
+        Cursor quoteCursor = getApplicationContext().getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI, null, "_id=?", new String[]{String.valueOf(quoteId)}, null);
+        StockDetailPresenter stockDetailPresenter = new StockDetailPresenter();
+        if (doesQuoteNotExist(quoteCursor))
+            return;
+        quoteCursor.moveToFirst();
+        String quoteSymbol = quoteCursor.getString(quoteCursor.getColumnIndex(QuoteColumns.SYMBOL));
+    }
+
+    private boolean doesQuoteNotExist(Cursor quote) {
+        return quote.getCount() == 0;
     }
 }
