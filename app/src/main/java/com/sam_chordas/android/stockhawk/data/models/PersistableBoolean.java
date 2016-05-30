0a1f028ffb654b93ac3243bda158f425df51191b
Chirag <me@chi6rag.net>
2016-05-30 08:42:00 +0530

    Adding parsing logic on PersistableBoolean

--- app/src/main/java/com/sam_chordas/android/stockhawk/data/models/PersistableBoolean.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/data/models/PersistableBoolean.java
@@ -1,6 +1,7 @@
 package com.sam_chordas.android.stockhawk.data.models;
 
 public enum PersistableBoolean {
+
     TRUE {
         public int value() {
             return 1;
@@ -14,5 +15,9 @@ public enum PersistableBoolean {
 
     public int value() {
         return this.value();
+    }
+
+    public static PersistableBoolean parse(int value) {
+        return value == 1 ? PersistableBoolean.TRUE : PersistableBoolean.FALSE;
     }
 }
--- app/src/test/java/com/sam_chordas/android/stockhawk/data/models/PersistableBooleanTest.java
+++ app/src/test/java/com/sam_chordas/android/stockhawk/data/models/PersistableBooleanTest.java
@@ -14,4 +14,19 @@ public class PersistableBooleanTest {
     public void testPersistableBooleanKnowsItsValueWhenItIsFalse() {
         assertEquals(0, PersistableBoolean.FALSE.value());
     }
+
+    @Test
+    public void testPersistableBooleanKnowsHowToParseFalseValue() {
+        assertEquals(PersistableBoolean.FALSE, PersistableBoolean.parse(0));
+    }
+
+    @Test
+    public void testPersistableBooleanKnowsHowToParseTrueValue() {
+        assertEquals(PersistableBoolean.TRUE, PersistableBoolean.parse(1));
+    }
+
+    @Test
+    public void testPersistableBooleanParsesValueAsFalseWhenNonTrueValue() {
+        assertEquals(PersistableBoolean.FALSE, PersistableBoolean.parse(1000));
+    }
 }
